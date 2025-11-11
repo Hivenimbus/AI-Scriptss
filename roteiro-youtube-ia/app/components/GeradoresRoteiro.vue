4e2781c618171b07fd25ec9b1186813b97ca6880
Hivenimbus <hivenimbus@gmail.com>
2025-11-11 16:54:21 -0300

    AA

--- roteiro-youtube-ia/app/components/GeradoresRoteiro.vue
+++ roteiro-youtube-ia/app/components/GeradoresRoteiro.vue
@@ -32,7 +32,7 @@
       >
         <div class="flex items-center justify-center gap-2">
           <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-            <path d="M5 4a2 2 0 012-2h6a2 2 0 012 2v14l-5-2.5L5 18V4z" />
+            <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" />
           </svg>
           <span>Seleção de Temas</span>
         </div>
